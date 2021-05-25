9f8bf7b933ec1fe6d32c7c49b93cbd07fd4ba976
jiruifu-jerry0219 <jiruif@hotmail.com>
2021-05-25 16:41:00 -0400

    emg_process

---
+++ emg_process/filter.py
@@ -0,0 +1,16 @@
+from scipy.signal import butter, lfilter
+import matplotlib.pyplot as pyplot
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
